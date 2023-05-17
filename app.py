c5b30c88c66ae230a93fc4e0225d5a42b0e6003f
David Rosenbloom <darosenbloom@gmail.com>
2023-05-16 21:07:51 -0400

    Given that no qualifying loans exist, when prompting a user to save a file, then the program should notify the user and exit.

--- app.py
+++ app.py
@@ -110,7 +110,9 @@ def save_qualifying_loans(qualifying_loans):
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
     # YOUR CODE HERE!
-
+    if len(qualifying_loans) == 0:
+        print(f"There are no loans that match the provided criteria")
+        return
 
 def run():
     """The main function for running the script."""
